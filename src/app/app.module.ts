24c75720124d3085c3f3c09dc46a3838c7ddb40e
diegominelli <diego.minelli@hotmail.com>
2020-09-10 14:08:03 -0300

    Criando o formulario para inserir no db.json um novo produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,8 +20,10 @@ import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule, } from '@angular/material/snack-bar';
-
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 
 
 
@@ -47,7 +49,10 @@ import { HttpClientModule } from '@angular/common/http'
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
