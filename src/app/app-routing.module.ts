30586008b20147011baa7f836f5563c87c350746
diegominelli <diego.minelli@hotmail.com>
2020-09-12 19:52:45 -0300

    Implementação da função deletar produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent} from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent} from './components/product/product-create/product-create.component'
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 const routes: Routes = [
   {
@@ -23,6 +24,11 @@ const routes: Routes = [
   {
     path: "product/update/:id",
     component: ProductUpdateComponent
+    
+  },
+  {
+    path: "product/delete/:id",
+    component: ProductDeleteComponent
     
   }
 
